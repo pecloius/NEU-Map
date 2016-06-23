da7f84b6e7e64ef4171e43035ae204579e34128e
chanPecko <peckoz@hotmail.com>
2016-06-23 10:54:41 +0700

    Initial Commit

---
+++ My Map NEUTests/My_Map_NEUTests.swift
@@ -0,0 +1,36 @@
+//
+//  My_Map_NEUTests.swift
+//  My Map NEUTests
+//
+//  Created by Pecko Loius on 6/23/16.
+//  Copyright © 2016 Pecko Loius. All rights reserved.
+//
+
+import XCTest
+@testable import My_Map_NEU
+
+class My_Map_NEUTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
